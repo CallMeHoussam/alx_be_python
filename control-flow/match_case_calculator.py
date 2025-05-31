608a1d90a1aa3efea7a79248bf53d8ed4bb36090
Evoless <amoogames519@gmail.com>
2025-05-31 18:12:05 +0100

    Edited match_case_operator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,19 +1,22 @@
 first = int(input("Enter the first number: "))
 second = int(input("Enter the second number: "))
 operator = input("Choose the operation (+, -, *, /): ")
-if operator == "*" :
-    result = first * second
-    print("The result is ",result)
-elif operator == "+" :
-    result = first + second
-    print("The result is ",result)
-elif operator == "-" :
-    result = first - second
-    print("The result is ",result)
-elif operator == "/" :
-    
-    if second == 0 :
-        print("Cannot divide by zero.")
-    else :
-        result = first / second
+match operator :
+    case "*" :
+        result = first * second
         print("The result is ",result)
+match operator :
+    case "+" :
+        result = first + second
+        print("The result is ",result)
+match operator :
+    case "-" :
+        result = first - second
+        print("The result is ",result)
+match operator :
+    case "/" :
+        if second == 0 :
+            print("Cannot divide by zero.")
+        else :
+            result = first / second
+            print("The result is ",result)
